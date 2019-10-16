88e1cd96bae1088fe4c9e6070c3f06e7f7b801b3
hanh <dovanhanh12b@gmail.com>
2019-10-16 18:32:42 +0700

    fix index render

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,16 +10,16 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/home.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('css/home.css') }}">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ secure_asset('js/home.js') }}"></script>
 </head>
 <body>
 <div id="app">
